dede09bcc544d0f7a5b9c021eaa1fac9d18b3aa4
poohlikung <118596123+poohlikung@users.noreply.github.com>
2026-09-21 12:57:48 +0700

    feat: create product client

---
+++ src/main/java/com/example/lab10/client/ProductWebClient.java
@@ -0,0 +1,61 @@
+package com.example.lab10.client;
+
+import com.example.lab10.model.Product;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class ProductWebClient {
+
+  
+    private final WebClient client = WebClient.create("http://localhost:8080");
+
+    public Mono<Product> getProductById(String id) {
+        return client.get()
+                .uri("/products/{id}", id)
+                .retrieve()
+                .bodyToMono(Product.class);
+    }
+
+    public Flux<Product> getAllProducts() {
+        return client.get()
+                .uri("/products")
+                .retrieve()
+                .bodyToFlux(Product.class);
+    }
+
+    public Mono<Product> createProduct(Product product) {
+        return client.post()
+                .uri("/products")
+                .bodyValue(product)
+                .retrieve()
+                .bodyToMono(Product.class);
+    }
+
+    public Mono<Void> deleteProduct(String id) {
+        return client.delete()
+                .uri("/products/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class);
+    }
+
+   
+    public Flux<Product> getByCategory(String category) {
+        return client.get()
+                .uri("/products/category/{category}", category)
+                .retrieve()
+                .bodyToFlux(Product.class);
+    }
+
+  
+    public Mono<Double> getDiscountedPrice(String id) {
+        return client.get()
+                .uri("/products/{id}/price", id)
+                .retrieve()
+                .bodyToMono(Double.class)
+                .doOnNext(price -> System.out.println("Price: " + price));
+    }
+}
