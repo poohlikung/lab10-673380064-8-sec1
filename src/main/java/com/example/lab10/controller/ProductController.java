bf44f8c6910322b524dcc4a7b3602693baecbe4b
poohlikung <118596123+poohlikung@users.noreply.github.com>
2026-09-21 12:53:37 +0700

    feat: product controller

---
+++ src/main/java/com/example/lab10/controller/ProductController.java
@@ -0,0 +1,55 @@
+package com.example.lab10.controller;
+
+import com.example.lab10.model.Product;
+import com.example.lab10.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+  
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Product> getById(@PathVariable String id) {
+        return service.getById(id);
+    }
+
+
+    @GetMapping
+    public Flux<Product> getAll() {
+        return service.getAll();
+    }
+
+ 
+    @PostMapping
+    public Mono<Product> save(@RequestBody Product product) {
+        return service.save(product);
+    }
+
+ 
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable String id) {
+        return service.delete(id);
+    }
+
+
+    @GetMapping("/category/{category}")
+    public Flux<Product> getByCategory(@PathVariable String category) {
+        return service.getByCategory(category);
+    }
+
+
+    @GetMapping("/{id}/price")
+    public Mono<Double> getDiscountedPrice(@PathVariable String id) {
+        return service.getDiscountedPrice(id);
+    }
+}
